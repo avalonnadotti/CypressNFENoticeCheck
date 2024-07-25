2f6126e3e6e089a3aa81271de1973e59afb5c220
avalonnadotti <nadottiavalon@gmail.com>
2024-07-25 01:48:28 -0300

    alteração no cy.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   video: false,
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
