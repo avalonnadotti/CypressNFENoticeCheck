f1120f7202cd58c820b359946a1aa79d505f9d44
avalonnadotti <nadottiavalon@gmail.com>
2024-07-25 01:24:37 -0300

    teste de check de novo informe de NFe

---
+++ cypress/e2e/firstSoloTest.spec.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Primeira stack de testes não guiada', ()=>{
+    it('Visita site da SEFAZ', () => {
+        cy.visit('https://www.nfe.fazenda.gov.br/portal/informe.aspx?ehCTG=false', {
+        
+        })
+        cy.get('.divInforme a').first().should('have.attr', 'name', '1291')
+    })
+})
